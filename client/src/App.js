7734477c426dc91957d1bde16afc9949f77bae3f
Egbe Alexander <alexsucceeds@gmail.com>
2023-03-09 17:36:54 +0100

    create chat page, body, footer and fix the bug on Home.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import { Routes, Route } from "react-router-dom";
+import Home from './components/Home';
+// import ChatPage from "./components/ChatPage";
+import ChatPage from "./components/ChatPage"
 import socketIO from "socket.io-client";
-import ChatPage from "./components/ChatPage";
-import Home from "./components/Home";
-const socket = socketIO.connect("http://localhost:4000");
 
+const socket = socketIO.connect("http://localhost:4000");
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home socket={socket} />} />
-        <Route path="/chat" element={<ChatPage socket={socket} />} />
+        <Route path="/" element={<Home socket={socket} />}></Route>
+        <Route path="/chat" element={<ChatPage socket={socket} />}></Route>
       </Routes>
     </div>
   );
---
+++ client/src/components/ChatBar.js
@@ -0,0 +1,18 @@
+const ChatBar = () => {
+  return (
+    <div className="chat__sidebar">
+      Open Chat
+      <div>
+        <h4 className="chat__header">ACTIVE USERS</h4>
+        <div>
+          <p>User 1</p>
+          <p>User 2</p>
+          <p>User 3</p>
+          <p>USer 4</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBar;
