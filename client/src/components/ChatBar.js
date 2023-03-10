4e3832504b22edf5e91623b0b77b0345a49464d8
Egbe Alexander <alexsucceeds@gmail.com>
2023-03-10 15:14:51 +0100

    update fetching function

--- client/src/components/ChatBar.js
+++ client/src/components/ChatBar.js
@@ -1,14 +1,21 @@
-const ChatBar = () => {
+import { useState, useEffect } from "react";
+
+const ChatBar = ({ socket }) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    socket.on("newUserResponse", (data) => setUsers(data));
+  }, [socket, users]);
+
   return (
     <div className="chat__sidebar">
       Open Chat
       <div>
         <h4 className="chat__header">ACTIVE USERS</h4>
         <div>
-          <p>User 1</p>
-          <p>User 2</p>
-          <p>User 3</p>
-          <p>USer 4</p>
+          {users.map((user) => (
+            <p key={user.socketID}> {user.userName} </p>
+          ))}
         </div>
       </div>
     </div>
--- client/src/components/ChatBody.js
+++ client/src/components/ChatBody.js
@@ -1,44 +1,46 @@
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const ChatBody = () => {
-  let navigate = useNavigate();
+const ChatBody = ({ messages }) => {
+  const navigate = useNavigate();
 
   const handleLeaveChat = () => {
-    localStorage.removeItem("userName");
-    navigate("/");
+    localStorage.removeItem('userName');
+    navigate('/');
     window.location.reload();
   };
+
   return (
     <>
       <header className="chat__mainHeader">
-        <p>Handgout with Colleagues</p>
+        <p>Hangout with Colleagues</p>
         <button className="leaveChat__btn" onClick={handleLeaveChat}>
           LEAVE CHAT
         </button>
       </header>
 
-      {/* This shows messages sent from you */}
       <div className="message__container">
-        <div className="message__chats">
-          <p className="sender__name">You</p>
-          <div className="message__sender">
-            <p>Hello there</p>
-          </div>
+        {messages.map((message) =>
+          message.name === localStorage.getItem('userName') ? (
+            <div className="message__chats" key={message.id}>
+              <p className="sender__name">You</p>
+              <div className="message__sender">
+                <p>{message.text}</p>
+              </div>
+            </div>
+          ) : (
+            <div className="message__chats" key={message.id}>
+              <p>{message.name}</p>
+              <div className="message__recipient">
+                <p>{message.text}</p>
+              </div>
+            </div>
+          )
+        )}
+
+        <div className="message__status">
+          <p>Someone is typing...</p>
         </div>
-      </div>
-
-      {/* This shows messages recevied by you */}
-
-      <div className="message__chats">
-        <p>Other</p>
-        <div className="message__recipient">
-          <p>Hey, I'm good, you ?</p>
-        </div>
-      </div>
-
-      {/* Thisis triggered when a user is typing */}
-      <div className="message__status">
-        <p>Someone is typing....</p>
       </div>
     </>
   );
