c6494312da3ec4ee7e51efacf7ee71099a773107
Egbe Alexander <alexsucceeds@gmail.com>
2023-03-16 00:06:45 +0100

    fix bug in chatBody.js

--- client/src/components/ChatFooter.js
+++ client/src/components/ChatFooter.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-const ChatFooter = () => {
-  const [message, setMessage] = useState("");
+const ChatFooter = ({ socket }) => {
+  const [message, setMessage] = useState('');
+
+  const handleTyping = () =>
+    socket.emit('typing', `${localStorage.getItem('userName')} is typing`);
 
   const handleSendMessage = (e) => {
     e.preventDefault();
-    console.log({ userName: localStorage.getItem("userName"), message });
-    setMessage("");
+    if (message.trim() && localStorage.getItem('userName')) {
+      socket.emit('message', {
+        text: message,
+        name: localStorage.getItem('userName'),
+        id: `${socket.id}${Math.random()}`,
+        socketID: socket.id,
+      });
+    }
+    setMessage('');
   };
-
   return (
     <div className="chat__footer">
       <form className="form" onSubmit={handleSendMessage}>
@@ -18,8 +27,9 @@ const ChatFooter = () => {
           className="message"
           value={message}
           onChange={(e) => setMessage(e.target.value)}
+                    // {/*OnKeyDown function*/}
+          onKeyDown={handleTyping}
         />
-
         <button className="sendBtn">SEND</button>
       </form>
     </div>
