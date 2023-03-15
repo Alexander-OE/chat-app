8411da2c5c7f4609f7598a076c94816efab37218
Egbe Alexander <alexsucceeds@gmail.com>
2023-03-15 23:32:42 +0100

    made some improvements

--- client/src/components/ChatBody.js
+++ client/src/components/ChatBody.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const ChatBody = ({ messages }) => {
+const ChatBody = ({ messages, lastMessageRef }) => {
   const navigate = useNavigate();
 
   const handleLeaveChat = () => {
@@ -12,35 +12,10 @@ const ChatBody = ({ messages }) => {
 
   return (
     <>
-      <header className="chat__mainHeader">
-        <p>Hangout with Colleagues</p>
-        <button className="leaveChat__btn" onClick={handleLeaveChat}>
-          LEAVE CHAT
-        </button>
-      </header>
-
-      <div className="message__container">
-        {messages.map((message) =>
-          message.name === localStorage.getItem('userName') ? (
-            <div className="message__chats" key={message.id}>
-              <p className="sender__name">You</p>
-              <div className="message__sender">
-                <p>{message.text}</p>
-              </div>
-            </div>
-          ) : (
-            <div className="message__chats" key={message.id}>
-              <p>{message.name}</p>
-              <div className="message__recipient">
-                <p>{message.text}</p>
-              </div>
-            </div>
-          )
-        )}
-
-        <div className="message__status">
-          <p>Someone is typing...</p>
-        </div>
+      <div>
+        ......
+        {/* --- At the bottom of the JSX element ----*/}
+        <div ref={lastMessageRef} />
       </div>
     </>
   );
